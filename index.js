2546d4967e155a9cd9dcc751c132341ee795de15
HevelMc <42890916+HevelMc@users.noreply.github.com>
2019-08-20 17:09:51 +0200

    Protect token #3

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
-const token = require("./token.json");
 const fs = require('fs');
 let prefix = config.prefix;
 client.commands = new Discord.Collection();
